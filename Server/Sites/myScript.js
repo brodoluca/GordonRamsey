17478e0de608ba7562e50f3b085ef1b3a482ce02
Luca Brodo <luca.brodo@stud.hshl.de>
2021-06-01 10:27:40 +0200

    now you can use the text bos

--- Server/Sites/myScript.js
+++ Server/Sites/myScript.js
@@ -1,3 +1,5 @@
+let truck_ip="localhost";
+let truck_port=4242;
 
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
@@ -6,24 +8,25 @@ const header = document.querySelector('header');
 const section = document.querySelector('section');
 
 function askForStatus(){
-
-    //the url is unfortunately local but whatever
-    let requestURL = 'http://localhost:4242'
-    //send the request to the truck
-    let request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    //when the truck responds, shit happens
-    request.onload = function() {
-        console.log(request.response);
-        const superHeroes = request.response;
-        populate(superHeroes);
-      }
-    request.send();
+    truck_ip = document.getElementById("ip");
+    truck_port = document.getElementById("port");
     
-
-    function hand () {
-        console.log(this);
+  if(truck_ip == null || truck_port == null){
+    document.getElementById("demo").innerHTML = "NOOOO"
+  }else{
+  //the url is unfortunately local but whatever
+  let requestURL = 'http://'+truck_ip.value+':'+truck_port.value;
+//send the request to the truck
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+//when the truck responds, shit happens
+request.onload = function() {
+    console.log(request.response);
+    const superHeroes = request.response;
+    populate(superHeroes);
+  }
+request.send();
 }
 
 
@@ -31,7 +34,10 @@ function askForStatus(){
 }
 function populate(obj) {
     //this should be made more beautiful
-    document.getElementById("demo").innerHTML = "wiktor"+'\n' + 'ID'+'\n'   +'ProcessID' +'\n'  +'Speed' ;
+    document.getElementById("demo").innerHTML = "Name = "+ obj['Name']+'<br />'+'<br />' +'ID = ' + 
+    obj['ID']+'<br />'+'<br />' +'ProcessID = '   +obj['ProcessID'] +'<br />'+'<br />' +'Speed = '  
+    +obj['Speed'] +'<br />'+'<br />' +'Size of the platoon = '  +obj['PlatoonSize'] +'<br />'+'<br />' +'Am I connected to the master = '  +obj['MasterConnection'] +'<br />'+'<br />' +'Port = '  +obj['port']+'<br />'+'<br />' 
+    +'Ip = '  +obj['ip'] +'<br />'+'<br />' +'Am I the master = '  +obj['master']   +'<br />'+'<br />' +'Probability to be a master = '  +obj['Probability'];
 
 
     //const myH1 = document.createElement('h1');
